0bc626b70919337be92657a400eb11a08ac5cea6
felicialeung <fwleung@uci.edu>
2025-05-24 10:45:05 -0700

    edited ShelfRow to create new Figure component and edited seriesData json file to have real_img

--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,12 +1,12 @@
-export default function Figure(props) {
+export default function Figure({ image, name }) {
     return (
-        <div>
-            This is a figure.
-            It will have an image and 
-            a small text of its name below.
-            Its data is passed in via props.
-            Grayscale (?) image if we know it's not found yet. 
-            maybe do: (props.show) ? (props.image) : (props.silhouette / silohuette that's imported so we don't need to pass in?)
+        <div className="w-24 flex-shrink-0 text-center text-white">
+            <img 
+                src={image} 
+                alt={name} 
+                className="w-full h-32 object-cover rounded" 
+            />
+            <p className="mt-2 text-sm gaegu-regular">{name}</p>
         </div>
     );
 }
--- src/components/ShelfRow.jsx
+++ src/components/ShelfRow.jsx
@@ -1,19 +1,15 @@
+import Figure from './Figure';
+
 export default function ShelfRow({ series }) {
     return (
         <div className="bg-[#9B6A59] p-4 rounded">
             <h2 className="gaegu-regular text-white mb-4 text-lg font-semibold">{series.name}</h2>
             <div className="flex space-x-4 overflow-x-auto">
                 {series.options.map((option, idx) => (
-                    <div key={idx} className="w-24 flex-shrink-0 text-center text-white">
-                        <img 
-                            src={option.image} 
-                            alt={option.name} 
-                            className="w-full h-32 object-cover rounded" 
-                        />
-                        <p className="mt-2 text-sm gaegu-regular">{option.name}</p>
-                    </div>
+                    <Figure key={idx} image={option.image} name={option.name} />
                 ))}
             </div>
         </div>
     );
 }
+
