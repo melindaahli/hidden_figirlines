75936534243b5f33bfa1ce826df61d51119070ea
felicialeung <fwleung@uci.edu>
2025-05-24 09:22:36 -0700

    added padding between "PLAY" and "STORE" in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import WideButton from '../components/WideButton.jsx'
 
 function Home(props) {
-    
-
     return (
-    <div>
-        This is the Home page.
-        {/* to do: 
-        1. what goes on top of the buttons?
-        2. add spacing between the buttons (try div wrapping them and searching 'tailwindcss vertical flex')
-        */}
-        <WideButton text={"PLAY"} clickFunction={() => props.setCurrentPage('game')}/>
-        <WideButton text={"OPEN"} clickFunction={() => props.setCurrentPage('store')}/>
-    </div>
+        <div>
+            This is the Home page.
+            {/* to do: 
+            1. what goes on top of the buttons?
+            2. add spacing between the buttons (try div wrapping them and searching 'tailwindcss vertical flex')
+            */}
+            <div className="flex flex-col space-y-4 mt-4">
+                <WideButton text={"PLAY"} clickFunction={() => props.setCurrentPage('game')} />
+                <WideButton text={"STORE"} clickFunction={() => props.setCurrentPage('store')} />
+            </div>
+        </div>
     );
 }
 
