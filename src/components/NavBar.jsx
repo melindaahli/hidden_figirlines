24a1b43707cd721fa411d7c72ab873695b940610
Melinda Li <melindaahli@gmail.com>
2025-05-23 23:21:00 -0700

    update website name and navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,36 @@
 function NavBar(props) {
 
     return (
+
         <div>
-            <button onClick={() => props.setCurrentPage('home')}>home</button>
-            <button onClick={() => props.setCurrentPage('game')}>game</button>
-            <button onClick={() => props.setCurrentPage('store')}>open</button>
-            <button onClick={() => props.ssetCurrentPage('shelf')}>shelf</button>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <button className="btn btn-ghost text-xl" 
+                            onClick={() => props.setCurrentPage('home')}>
+                                figirlines
+                    </button>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <button onClick={() => props.setCurrentPage('game')}>game</button>
+                        </li>
+                        <li>
+                            <button onClick={() => props.setCurrentPage('store')}>open</button>
+                        </li>
+                        <li>
+                            <button onClick={() => props.setCurrentPage('shelf')}>shelf</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">coins {props.currency}</a>
+                </div>
+            </div>
+
+
+
+
         </div>
     );
 }
