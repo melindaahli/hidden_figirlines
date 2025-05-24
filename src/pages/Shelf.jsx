fff9558808e5e729ea0b07902f08d3c0dea07877
felicialeung <fwleung@uci.edu>
2025-05-24 10:13:26 -0700

    created bookshelf with fake series

--- src/pages/Shelf.jsx
+++ src/pages/Shelf.jsx
@@ -1,12 +1,23 @@
-import BookShelf from '../components/BookShelf'
+import BookShelf from '../components/BookShelf';
+import data from '../../seriesData.json';
+
+// function Shelf() {
+//   return (
+//     <div className="bg-[#FAF8F0] min-h-screen p-6">
+//       <h1 className="text-[#794F41] text-4xl font-bold text-center mb-8">Bookshelf</h1>
+//       <BookShelf data={data} />
+//     </div>
+//   );
 
 function Shelf() {
-    return (
-        <div>
-            This is the Shelf page.
-            <BookShelf />
-        </div>
-    );
+  return (
+    <div className="bg-[#FAF8F0] min-h-screen p-6">
+      <h1 className="gaegu-regular text-[#794F41] text-4xl font-bold text-center mb-8">
+        Bookshelf
+      </h1>
+      <BookShelf data={data} />
+    </div>
+  );
 }
 
 export default Shelf;
