f6adf9b105aeaaba063ba6f9c433d9a5216d7144
Melinda Li <melindaahli@gmail.com>
2025-05-24 20:37:03 -0700

    only collected figures show

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -2,7 +2,7 @@ import ShelfRow from './ShelfRow';
 
 export default function BookShelf({ data, collectedFigures }) {
     return (
-        <div className="bg-[#794F41] p-4 space-y-6">
+        <div className="bg-[#794F41] p-6 space-y-6 rounded-2xl w-[50vw]">
             {data.map((series, index) => (
                 <ShelfRow key={index} series={series} collectedFigures={collectedFigures}/>
             ))}
